37721aa3abbef90270378138b72408d116a46565
Kerim Öztürk <kerimmozturk@hotmail.com>
2025-10-17 14:38:55 +0300

    java

---
+++ 11aDers/src/Main.java
@@ -0,0 +1,250 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("KErim");
+        //System.out.print("öztürk");
+        //System.out.print("11B dersi Çğretmeni");
+        //System.out.print("'dir");
+        //System.out.println("merhaba");
+        //System.out.print("gencler");
+
+
+
+        // System.out.println("-------- Yemek Çeşitleri ---------");
+        // System.out.println();
+        // System.out.println("1- Ana Yemek");
+        // System.out.println("2- Çorbalar");
+        // System.out.println("3-  Tatlılar");
+        // System.out.println("4- Salatalar");
+        // System.out.println("5- İçecekler");
+        // System.out.println();
+        // System.out.println("-------- Yemek Çeşitleri ---------");
+
+
+        //Snake Case => degisken_turu_ornegi
+        //Camel Case => degiskenTuruOrnegi
+        //Pascal Case => DegiskenTuruOrnegi
+        // Hungarian Case=> strDegiskenTuruOrnegi
+        // Keabab Caee => degisken-turu-ornegi
+
+        //String yolcuAdi="Nazmi";
+        //String YolcuSoyAdı="Şen";
+        //String yolcueposta="nazmisen@hotmail.com";
+        //String yolcutelefonnumara="12321312";
+        //String yolcuLokasyon= "Gaziosmanpaşa";
+        //String yolcuSehir="istanbul";
+
+        //System.out.println("----------Rezervasyon KArtı--------");
+        //System.out.println();
+        //System.out.println("Müşteri Adı: "+ yolcuAdi    );
+        //System.out.println("Müşteri Soyadı: "+ YolcuSoyAdı);
+        //System.out.println("Müşteri Epostası: "+ yolcueposta);
+        //System.out.println("Müşteri Telefon no: "+ yolcutelefonnumara);
+        //System.out.println("Müşteri'nin Yaşadığı yer:  "+ yolcuLokasyon);
+        //System.out.println("Adres: "+ yolcuSehir+ "/"+ yolcuLokasyon);
+        //System.out.println();
+        //System.out.println("----------Rezervasyon KArtı--------");
+
+
+       /* int hamburgerFiyarı=300;
+        int KolaFiyatı=80;
+        int suFiyatı=10;
+        int PatatesKızartmasıFiyatı=100;
+        int LimonataFiyatı=100;
+
+        int hamburgerSayisi=2;
+        int kolaSAyisi=2;
+        int suSayisi=2;
+        int PatatesKızartması=2;
+        int LimonataSayisi=1;
+
+        int toplamHamburgerFiyatı= hamburgerFiyarı*hamburgerSayisi;
+        int toplamKolaFiyatı=kolaSAyisi*KolaFiyatı;
+        int toplamSusFiyatı=suFiyatı*suSayisi;
+        int toplamPatatesKızartmasıFiyatı=PatatesKızartmasıFiyatı*PatatesKızartması;
+        int toplamLimonataFiyatı=LimonataFiyatı*LimonataSayisi;
+
+
+        System.out.println("************ Sipariş Özeti **********");
+        System.out.println();
+        System.out.println("Hamburger: "+ toplamHamburgerFiyatı +" TL");
+        System.out.println("Kola :"+toplamKolaFiyatı +" TL");
+        System.out.println("Su: "+toplamSusFiyatı +" TL");
+        System.out.println("Patates Kızartma: "+toplamPatatesKızartmasıFiyatı +" TL");
+        System.out.println("Limonata Fiyatı: "+toplamLimonataFiyatı +" TL");
+
+
+        double portakalFiyati=12.43;
+        double elmaFiyatı=23.51;
+        double muzFiyatı=100.99;
+        double uzumFiyatı=130.2;
+        double cilekFiyatı=7.42;
+
+        System.out.println("------------ Fiyat Listesi ***********");
+        System.out.println();
+        System.out.println(" Portakal : "+ portakalFiyati +" TL");
+        System.out.println(" Muz : "+muzFiyatı+" TL");
+        System.out.println(" Üzüm Fiyatı : "+uzumFiyatı+ " TL");
+        System.out.println(" Çilek Fiyatı : "+cilekFiyatı+" TL");
+
+*/
+/*
+
+        double portakalFiyati=12.43;
+        double elmaFiyatı=23.51;
+        double muzFiyatı=100.99;
+        double uzumFiyatı=130.2;
+        double cilekFiyatı=7.42;
+
+
+        int portakalKg=1;
+        int elmaKG=2;
+        int muzKg=3;
+        int uzumKG=4;
+        int cilekKg=5;
+
+        double toplamPortakalFiyati=portakalFiyati*portakalKg;
+        double toplamElmaFiyatı=elmaFiyatı*elmaKG;
+        double toplamMuzFiyatı=muzFiyatı*muzKg;
+        double toplamUZumFiyatı=uzumFiyatı*uzumKG;
+        double toplamCilekFiyatı=cilekFiyatı*cilekKg;
+
+        System.out.println("------------ Fiyat Listesi ***********");
+        System.out.println();
+        System.out.println(" Portakal : "+ toplamPortakalFiyati +" TL");
+        System.out.println(" Muz : "+toplamMuzFiyatı+" TL");
+        System.out.println(" Üzüm Fiyatı : "+toplamUZumFiyatı+ " TL");
+        System.out.println("Elma Fiyatı: "+toplamElmaFiyatı+" TL");
+        System.out.println(" Çilek Fiyatı : "+toplamCilekFiyatı+" TL");
+
+
+*/
+
+        Scanner input = new Scanner(System.in);
+
+        /*System.out.println("Lütfen Şifrreyi giriniz:");
+        String sifre=input.nextLine();
+
+        if (sifre.equals("abcd")||sifre.equals("ABCD")){
+            System.out.println("Girilen Şifre Doğrıdur.Sisteme Giriş yapılıryor.");
+        }
+        else {
+            System.out.println("Girilen şifre veya kullanıcı adi hatalı.");
+
+        }*/
+
+       /* System.out.println("Başkenti giriniz: ");
+        String baskent=input.nextLine();
+        System.out.println("Ülke giriniz:");
+        String ulke=input.nextLine();
+
+        if (baskent.equals("Ankara") && ulke.equals("Türkiye")){
+            System.out.println("Veriler doğrudur.");
+        }else {
+            System.out.println("Veriler yanlıştır");
+        }
+
+*/
+/*
+
+        System.out.println("Bir sayı giriniz: ");
+        int sayi= input.nextInt();
+
+        if(sayi==4){
+            System.out.println("SAyı dpğru tahmin edildi.");
+        }else {
+            System.out.println("Sayı yanlış tahmin edildi.");
+        }
+
+
+*/
+
+        /*System.out.println("1. Sınav notunu giriniz:");
+        double sinav1= input.nextDouble();
+        System.out.println("2. Sınav notunugiriniz");
+        double sinav2= input.nextDouble();
+        System.out.println("3. Sınab notunu giriniz:");
+        double sinav3= input.nextDouble();
+
+        double ortalama =(sinav1+sinav2+sinav3)/3;
+
+        String sonuc="";
+
+        if (ortalama>0 && ortalama<50){
+            sonuc="Sonuc Rezalet";
+
+        }
+        if (ortalama>=50 && ortalama<70){
+            sonuc="Sonuç ortalama";
+        }
+        if (ortalama>70 && ortalama<=84){
+            sonuc="Sonuç İYi";
+        }
+        if (ortalama>84 ){
+            sonuc="Sonuç mükkemmel";
+        }
+
+        System.out.println("Ortalama : "+ortalama+ "=====> "+ sonuc);
+
+
+        System.out.println("Lütfen şehir giriniz");
+        String sehir=input.nextLine();
+
+        if (sehir.equals("Trabzon") || sehir.equals("Ordu") || sehir.equals("Giresun")){
+            System.out.println("Sehir karadeniz bölgesindedir");
+        }
+        else {
+            System.out.println("Girilen şehir karadenizde değildir.");
+        }
+
+        System.out.println("Takım sembolünü giriniz:");
+        char TakimSembol= input.next().charAt(0);
+
+        if (TakimSembol== 'g' || TakimSembol=='G'){
+        System.out.println("Galatasaray");
+        }
+        if (TakimSembol== 'f' || TakimSembol=='F'){
+            System.out.println("Fenerbahce");
+        }
+        if (TakimSembol== 'b' || TakimSembol=='B'){
+            System.out.println("Besiktaş");
+        }*/
+
+        System.out.println("11-a Eğtim Restoranı");
+        System.out.println("-------------------");
+
+    System.out.println("1 - Ana yemek");
+    System.out.println("2 - Çorbalar");
+    System.out.println("3 - Pizzalar");
+    System.out.println("4 - İçecekler");
+    System.out.println("5 - Tatlılar");
+
+        System.out.println("-------------------");
+
+        input.nextLine();
+
+        System.out.println("Menü numarası seçiniz");
+        String menuSecim=input.nextLine();
+
+        if (menuSecim.equals("1")){
+           System.out.println("Köri Soslu tavuk, Tavuk pilav, Fasulye pilav");
+        }
+        else if (menuSecim.equals("2")){
+            System.out.println("mervimek çorbası, kelle paça, işkembe, tarhana");
+        }
+        else if (menuSecim.equals("3")){
+            System.out.println("Kuşbaşı Pizza, Ton balıklı pizza, karısık pizza ,pepperoni pizza");
+        }
+        else if (menuSecim.equals("4")){
+            System.out.println("Kola,fanta,gazoz,çay,sprite");
+        }
+        else if (menuSecim.equals("5")){
+            System.out.println("soğuk baklaca ,halka,kemalpaşa,baklava,künefe");
+        }
+
+
+
+
+    }
+}
---
+++ 11bDers/src/Main.java
@@ -0,0 +1,223 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("KErim");
+        //System.out.print("öztürk");
+        //System.out.print("11B dersi Çğretmeni");
+        //System.out.print("'dir");
+        //System.out.println("merhaba");
+        //System.out.print("gencler");
+
+
+
+       // System.out.println("-------- Yemek Çeşitleri ---------");
+       // System.out.println();
+       // System.out.println("1- Ana Yemek");
+       // System.out.println("2- Çorbalar");
+       // System.out.println("3- Tatlılar");
+       // System.out.println("4- Salatalar");
+       // System.out.println("5- İçecekler");
+       // System.out.println();
+       // System.out.println("-------- Yemek Çeşitleri ---------");
+
+
+        //Snake Case => degisken_turu_ornegi
+        //Camel Case => degiskenTuruOrnegi
+        //Pascal Case => DegiskenTuruOrnegi
+        // Hungarian Case=> strDegiskenTuruOrnegi
+        // Keabab Caee => degisken-turu-ornegi
+
+       /* String yolcuAdi="Nazmi";
+        String YolcuSoyAdı="Şen";
+        String yolcueposta="nazmisen@hotmail.com";
+        String yolcutelefonnumara="12321312";
+        String yolcuLokasyon= "Gaziosmanpaşa";
+        String yolcuSehir="istanbul";
+
+        System.out.println("----------Rezervasyon KArtı--------");
+        System.out.println();
+        System.out.println("Müşteri Adı: "+ yolcuAdi    );
+        System.out.println("Müşteri Soyadı: " + YolcuSoyAdı);
+        System.out.println("Müşteri Epotası:"+ yolcueposta);
+        System.out.println("Öüşteri Telefon numarası: "+yolcutelefonnumara);
+        System.out.println("Lokasyon : "+ yolcuLokasyon);
+        System.out.println(("ADres: "+ yolcuSehir+"/"+yolcuLokasyon));
+        System.out.println();
+        System.out.println("----------Rezervasyon KArtı--------");*/
+
+       /* int hamburherFiyatı=300;
+        int kolaFiyatı=80;
+        int suFiyatı=10;
+        int PatatesKızartmasıFiyatı=100;
+        int limonataFiyatı=100;
+
+        System.out.println("---------- 11 B MENU---------");
+        System.out.println();
+        System.out.println("Hamburger Fiyatı : "+ hamburherFiyatı +" TL");
+        System.out.println("Kola Fiyatı :"+kolaFiyatı+" TL");
+        System.out.println("Su Fiyatı: "+suFiyatı+" TL");
+        System.out.println("Patates Kızartması Fiyarı:"+PatatesKızartmasıFiyatı+" TL");
+        System.out.println("Limomanata Fiyatı:"+limonataFiyatı+" TL");
+        System.out.println();
+        System.out.println("---------- 11 B MENU---------");*/
+
+
+     /*   double elmaFiyatı=12.5;
+        double portakalFiyatı=15.99;
+        double cilekFiyatı=29.6443;
+        double muzFiyatı=99.86;
+        double domatesFiyatı= 29.32;
+
+        System.out.println("---------- Fiyat Listesi ----------");
+        System.out.println();
+        System.out.println("Elma : "+ elmaFiyatı+ " TL");
+        System.out.println("Portakal : "+portakalFiyatı+ " TL");
+        System.out.println("Çilek : "+ cilekFiyatı+" Tl");
+        System.out.println("Muz : "+ muzFiyatı+" TL");
+        System.out.println("Domates: "+ domatesFiyatı+" TL");
+        System.out.println();
+        System.out.println("---------- Fiyat Listesi ----------");
+
+        int elmaKg=2;
+        int portakalKg=3;
+        int cilekKg=4;
+        int muzKg=8;
+        int domatesKg=1;
+
+
+        double toplamELmaFiyatı=elmaFiyatı*elmaKg;
+        double toplamPortakalFiyatı=portakalFiyatı*portakalKg;
+        double toplamCilekFiyatı=cilekFiyatı*cilekKg;
+        double toplamMuzFiyatı=muzFiyatı*muzKg;
+        double toplamDomatesFiyatı=domatesFiyatı*domatesKg;
+        double toplamFisTutarı=toplamDomatesFiyatı+toplamCilekFiyatı+toplamMuzFiyatı+toplamPortakalFiyatı+toplamELmaFiyatı;
+
+        System.out.println("----------- Sipariş Özeti -----------");
+        System.out.println();
+        System.out.println("Toplam Elma Fiyatı :"+toplamELmaFiyatı+ " TL");
+        System.out.println("Toplam Portakal Fiyatı: "+toplamPortakalFiyatı+" TL");
+        System.out.println("Toplam Çilek Fiyatı: "+toplamCilekFiyatı+" TL");
+        System.out.println("Toplam Muz Fiyatı:"+toplamMuzFiyatı+" TL");
+        System.out.println("Toplam Domates Fiyatı"+ toplamDomatesFiyatı);
+        System.out.println("Toplam Tutar = "+toplamFisTutarı+ " TL");
+        System.out.println();
+        System.out.println("----------- Sipariş Özeti -----------");*/
+
+Scanner input= new Scanner(System.in);
+      /*  System.out.println(("Lütfen şifreyi giriniz :"));*/
+/*Koşullu ifadeler (İf/else)*/
+
+      /*  String sifre = input.nextLine();
+
+        if (sifre.equals("abcd")|| sifre.equals("ABCD"))
+        {
+            System.out.println("Girilen şifre doğrudur. Sisteme giriş yapılıryor");
+        }else {
+            System.out.println("Girilen kullanıcı veya şifre hatalıdır.");
+        }
+        */
+
+        /*System.out.println("Başkenti giriniz");
+        String baskent=input.nextLine();
+        System.out.println("Ülke giriniz:");
+        String ulke=input.nextLine();
+
+        if (baskent.equals ("Ankara") && ulke.equals("Türkiye")){
+            System.out.println("Veriler doğru");
+        }else{
+            System.out.println("Veriler Yanlıştır.");
+        }
+    */
+ /*       System.out.println("Bir sayı giriniz:");
+        int sayi=input.nextInt();
+
+        if (sayi==4){
+            System.out.println("sayı dpğru tahmin edildi.");
+        }else {
+            System.out.println("Sayı yanlış tahmin eddilmiştir.");
+        }*/
+
+
+// hafta 5-6 örnek
+//        System.out.println("1. Sınav notunu giriniz:");
+//        double sinav1=input.nextDouble();
+//        System.out.println("2.Sınav notunu giriniz:");
+//        double sinav2=input.nextDouble();
+//        System.out.println("3.sınav notunu giriniz.");
+//        double sinav3=input.nextDouble();
+//
+//        double ortalama = (sinav1+sinav2+sinav3)/3;
+//        String sonuc = "";
+//
+//        if (ortalama>=0 &&  ortalama<50){
+//            sonuc="Sonuç Rezalet";
+//        }
+//        if (ortalama>70 && ortalama<=84){
+//            sonuc="Sonuc iyi";
+//
+//        }
+//        if (ortalama>84){
+//            sonuc="Sonuc Mükemmel";
+//        }
+//
+//        System.out.println("Ortalama: "+ ortalama + "===>" + sonuc);
+//
+//
+////
+////
+//
+//        System.out.print("Lütfen şehir giriniz:");
+//        String sehir=input.nextLine();
+//
+//        if (sehir.equals("Trabzon")|| sehir.equals("Ordu")||sehir.equals("Giresun")){
+//            System.out.println("Girilen şehir Karadeniz Bölgesindedir.");
+//        }else {
+//            System.out.println("Girlen bölge karadeniz bölgesinde değildir.");
+//        }
+//
+//
+//        System.out.println("Takım sembolümü giriniz:");
+//        char takimSembol=input.next().charAt(0);
+//
+//        if (takimSembol=='g'||takimSembol=='G'){
+//            System.out.println("GALATASARAY");
+//        }
+//        if (takimSembol=='f'||takimSembol=='F'){
+//            System.out.println("FENERBAHÇE");
+//        }
+
+        System.out.println("11-B eğtim restoranı");
+        System.out.println("--------");
+        System.out.println("1- Ana yemek");
+        System.out.println("2- Çorba");
+        System.out.println("3- Pizzalar");
+        System.out.println("4- İçeçekler");
+        System.out.println("5 - tatlıalr");
+        System.out.println("----------");
+
+
+        input.nextLine();
+        System.out.println("Lütfen bir menü numarası giriniz.");
+        String menuSecim=input.nextLine();
+
+        if (menuSecim.equals("1")){
+            System.out.println("Ana yemekler : Köri soslu tavuk, makarna, tavuk pilav, döner");
+        } else if (menuSecim.equals("2")) {
+            System.out.println("Çorbalar: mercimek , kelle paça ,tarhana,işmebve");
+
+        }  else if (menuSecim.equals("3")) {
+            System.out.println("Pizzalar: kaşarlı pizza , sucuklı pizza ,kavurmalı pizza");
+
+        } else if (menuSecim.equals("4")) {
+            System.out.println("içecekler: kola , fabta ,çay,su");
+
+        } else if (menuSecim.equals("5")) {
+            System.out.println("tatlılar: şam , baklava  ,soğuk baklava,halka");
+
+        }
+
+
+    }
+
+}
